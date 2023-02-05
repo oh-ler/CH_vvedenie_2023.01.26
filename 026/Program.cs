c9a673a7f265312e7bb583900ef608d412a43114
Valeria <valeri.992bk.ru@ya.ru>
2023-02-05 10:44:58 +0300

    дз к семинару 2(исправила 026)

--- 026/Program.cs
+++ 026/Program.cs
@@ -9,3 +9,8 @@ c=Convert.ToInt32(a);
 b=Convert.ToInt32(a);
 int d=c/10;
 int e=c%10;
+int f=(c%1000)/10;
+if (d==e $$ (f%11==0 || f==0))
+    System.Console.WriteLine("палиндром");
+    else 
+    System.Console.WriteLine("не палиндром");
