d306622a6be022865146d49cafd8777314f06fa7
Валерия Алексеевна <valeriaalekseevna@MacBook-Pro-Valeria.local>
2023-02-26 17:00:31 +0300

    последний урок

---
+++ 098/Program.cs
@@ -0,0 +1,31 @@
+﻿//98. Показать треугольник Паскаля
+//*Сделать вывод в виде равнобедренного треугольника
+
+ int n = 10;
+    int[,] arr = new int[n, n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if (j == 0 || j == i)
+            {
+                arr[i, j] = 1;
+            }
+            else
+            {
+                arr[i, j] = arr[i - 1, j - 1] + arr[i - 1, j];
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = n - i; k > 0; k--)
+        {
+            Console.Write(" ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
